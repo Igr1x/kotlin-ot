151d1219001629c101d2dcf4fb1bdc71a080ed8f
Igr1x <varnavskii.igrix@mail.ru>
2024-11-09 21:54:16 +0300

    m1l1: first hw

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "kotlin-otus"
+rootProject.name = "otus-kotlin"
+
+include("m1l1-first")
 
